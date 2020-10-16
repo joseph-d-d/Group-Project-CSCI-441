dbaf9a05fd081d8fe0e0583f4ce405022c3c83ad
SethW05 <56840032+SethW05@users.noreply.github.com>
2020-10-15 23:15:03 -0500

    Update ParkingLot.js

--- src/parkingLot/ParkingLot.js
+++ src/parkingLot/ParkingLot.js
@@ -6,8 +6,8 @@ class ParkingLot {
   //TODO
   //FINDS CLOSEST SPOT AVAILALBLE TO SELECTED STORE
   reserveSpot(store) {
-	let xStore = 0; //will update to get x coordinate from store in database
-	let yStore = 0; //will update to get y coordinate from store in database
+	let xStore = 0; //get x coordinate where name equals "store"
+	let yStore = 0; //get y coordinate where name equals "store"
 	let distanceTemp = 999;
 	let xSpot = 0;
 	let ySpot = 0;
@@ -18,8 +18,8 @@ class ParkingLot {
 	
 	//FOR EXAMPLE, THE FOLLOWING ARE SPOTS THAT WERE MARKED AVAILALBLE FROM DATABASE
 	//PAIRS ARE (0,0) (5,2) (10,15) etc...
-	let xAvailSpots = [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]; //will update to get x coordinates from available spots in database
-	let yAvailSpots = [0,2,15,75,35,23,96,12,89,86,58,24,53,15,74,73,85,13,53,74,52]; //will update to get x coordinates from available spots in database
+	let xAvailSpots = [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]; //get x coordinate where status equals "available"
+	let yAvailSpots = [0,2,15,75,35,23,96,12,89,86,58,24,53,15,74,73,85,13,53,74,52]; //get y coordinate where status equals "available"
 					  
 	//USE DISTANCE FORMULA TO FIND CLOSEST AVAILALBLE SPOT TO THE SELECTED "store"
 	for (i = 0; i < availSpots.length; i++){
@@ -29,14 +29,15 @@ class ParkingLot {
 		}
 	}
 	
-	//GET parkingSpotID THATS ATTACHED TO THE x AND y VALUE
+	//get name where x equals xSpot and where y equals ySpot
 	//TODO
       	let parkingSpotID = 1;
       	return parkingSpotID;
   }
   //TODO
   isParkingSpotAvailable() {
-    return true;
+	//if spot status equals "available"
+    	return true;
   }
 }
 
