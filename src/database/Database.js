f7edc0b2be47697e476c45daf323a8d65a3ebf52
Tony B <25971310+MrBanh@users.noreply.github.com>
2020-10-28 05:02:47 -0700

    Update Database class addUser method to add document to 'users' collection

--- src/database/Database.js
+++ src/database/Database.js
@@ -4,80 +4,80 @@ var MongoClient = require("mongodb").MongoClient;
 var MongoUri = require("../constants/Keys").MONGODB_URI;
 
 class Database {
-  constructor() {
-    MongoClient.connect(MongoUri, (err, db) => {
-      if (!err) {
-        this.db = db.db("test");
-        console.log("We are connected");
-      }
-    });
-  }
-
-  addReservation = (reservation) => {
-    this.db.createCollection(
-      "reservations",
-      { strict: true },
-      (err, collection) => {}
-    );
-    var collection = this.db.collection("reservations");
-    collection.insertOne(reservation);
-  };
-
-  getReservation = async (userEmail) => {
-    var collection = this.db.collection("reservations");
-    var reservation = await collection.findOne({ email: userEmail });
-    console.log(reservation);
-    return reservation;
-  };
-
-  getReservations = async () => {
-    var collection = this.db.collection("reservations");
-    var cursor = collection.find();
-    var reservations = [];
-    while (await cursor.hasNext()) {
-      reservations.push(await cursor.next());
+    constructor() {
+        MongoClient.connect(MongoUri, (err, db) => {
+            if (!err) {
+                this.db = db.db("test");
+                console.log("We are connected");
+            }
+        });
     }
-    return reservations;
-  };
 
-  deleteReservation = () => {};
+    addReservation = (reservation) => {
+        this.db.createCollection(
+            "reservations",
+            { strict: true },
+            (err, collection) => {},
+        );
+        var collection = this.db.collection("reservations");
+        collection.insertOne(reservation);
+    };
 
-  getUserById = (id) => {
-    let result;
-    let collection = this.db.collection("users");
-    result = collection.findOne({ _id: id }, function (err, val) {
-      if (err) return null;
-      else return val;
-    });
+    getReservation = async (userEmail) => {
+        var collection = this.db.collection("reservations");
+        var reservation = await collection.findOne({ email: userEmail });
+        console.log(reservation);
+        return reservation;
+    };
 
-    return result;
-  };
+    getReservations = async () => {
+        var collection = this.db.collection("reservations");
+        var cursor = collection.find();
+        var reservations = [];
+        while (await cursor.hasNext()) {
+            reservations.push(await cursor.next());
+        }
+        return reservations;
+    };
 
-  getUsers = async () => {
-    var collection = this.db.collection("users");
-    var cursor = collection.find();
-    var users = [];
-    while (await cursor.hasNext()) {
-      users.push(await cursor.next());
-    }
-    return users;
-  };
+    deleteReservation = () => {};
 
-  addUser = (user) => {
-    this.db.createCollection("user", { strict: true }, (err, collection) => {});
-    var collection = this.db.collection("user");
-    collection.insertOne(user);
-  };
+    getUserById = async (id) => {
+        let result;
+        let collection = this.db.collection("users");
+        result = await collection.findOne({ _id: ObjectId(id) });
+        return result;
+    };
 
-  addParkingLot = async (parkingLot) => {
-    this.db.createCollection(
-      "parkingLot",
-      { strict: true },
-      (err, collection) => {}
-    );
-    var collection = this.db.collection("parkingLot");
-    collection.insertOne(parkingLot);
-  };
+    getUsers = async () => {
+        var collection = this.db.collection("users");
+        var cursor = collection.find();
+        var users = [];
+        while (await cursor.hasNext()) {
+            users.push(await cursor.next());
+        }
+        return users;
+    };
+
+    addUser = (user) => {
+        this.db.createCollection(
+            "users",
+            { strict: true },
+            (err, collection) => {},
+        );
+        var collection = this.db.collection("users");
+        collection.insertOne(user);
+    };
+
+    addParkingLot = async (parkingLot) => {
+        this.db.createCollection(
+            "parkingLot",
+            { strict: true },
+            (err, collection) => {},
+        );
+        var collection = this.db.collection("parkingLot");
+        collection.insertOne(parkingLot);
+    };
 }
 
 module.exports = Database;
