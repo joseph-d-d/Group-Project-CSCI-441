25f0715660e6edb06fdb302375e538e5b6ccd64b
Tony B <25971310+MrBanh@users.noreply.github.com>
2020-10-31 21:23:35 -0700

    feat: Add middleware.js

---
+++ src/middleware/middleware.js
@@ -0,0 +1,28 @@
+const middlewareObj = {};
+
+middlewareObj.userExists = async function(req, res, next){
+    // User is logged in and sent a PUT request to /dashboard to update
+    if (req.isAuthenticated() && req.body.email === req.user.email) {
+        return next(); // no need to check db, user did not change their email
+    }
+
+    // Otherwise, check db to see if there's already an account registered with the provided email
+    const userExist = (await req.app.locals.user.getUserByEmail(req.body.email))
+        ? true
+        : false;
+
+    if (userExist) {
+        // If so, send back 403 status code
+        res.status(403);
+        res.json(req.body.email);
+    } else {
+        return next();
+    }
+};
+
+middlewareObj.emailToLowerCase = (req, res, next) => {
+    req.body.email = req.body.email.toLowerCase();
+    return next();
+};
+
+module.exports = middlewareObj;
