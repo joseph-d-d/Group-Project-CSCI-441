2c4567568ccd4bcc9a95e35b6a4550bae462daff
Tony B <25971310+MrBanh@users.noreply.github.com>
2020-10-31 21:34:15 -0700

    feat: update class method for hashing, create new methods for updating user in db, authenticating user, serialize/deserialize user session

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,28 +3,32 @@ const { NotExtended } = require("http-errors");
 var router = express.Router();
 var User = require("../user/User");
 
-router.get("/:id", async function (req, res) {
-  let user;
-  try {
-    user = await req.app.locals.user.getUserById(req.params.id);
-    if ( user == null ) {
-      return res.status(404).json({message: "Unable to locate user"});
+// Used to pass data to dashboard html file, since GET /dashboard sends just html file without user data
+router.get("/loggedIn", function(req, res){
+    if (req.isAuthenticated()) {
+        const { password, ...user } = req.user;
+        res.json(user);
+    }
+});
+
+router.get("/:id", async function(req, res){
+    let user;
+    try {
+        user = await req.app.locals.user.getUserById(req.params.id);
+        if (user == null) {
+            return res.status(404).json({ message: "Unable to locate user" });
+        }
+
+        return res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 
-    return res.json(user);
-  }
-  catch ( error ) {
-    res.status(500).json({ message: error.message });
-  }
-  
-  return res.send(user);
+    return res.send(user);
 });
 
-router.get("/", function (req, res) {
-  req.app.locals.user
-    .getUsers()
-    .then((users) => res.json(users));
+router.get("/", function(req, res){
+    req.app.locals.user.getUsers().then((users) => res.json(users));
 });
-
 
 module.exports = router;
--- src/user/User.js
+++ src/user/User.js
@@ -1,31 +1,82 @@
 class User {
-  constructor(db) {
-    this.db = db;
-  }
+    constructor(db) {
+        this.db = db;
+    }
 
-  getUserById(id) {
-    return this.db.getUserById(id);
-  }
+    getUserById(id) {
+        return this.db.getUserById(id);
+    }
 
-  getUsers() {
-    return this.db.getUsers();
-  }
+    getUserByEmail(email) {
+        return this.db.getUserByEmail(email);
+    }
 
-  //Just an example of the user collection structure
-  addUser({firstName, lastName, phone, email, password, permissions, modified_date, modified_by, paymentMethod, vehicles}) {
-    this.db.addUser({
-      firstName: firstName,
-      lastName: lastName,
-      phone: phone,
-      email: email,
-      password: password,
-      permissions: permissions,
-      modified_date: modified_date,
-      modified_by: modified_by,
-      paymentMethod: paymentMethod,
-      vehicles: vehicles
-    });
-  }
+    getUsers() {
+        return this.db.getUsers();
+    }
+
+    addUser({
+        firstName,
+        lastName,
+        phone,
+        email,
+        password,
+        permissions,
+        modified_date,
+        modified_by,
+        paymentMethod,
+        vehicles,
+    }) {
+        this.db.addUser({
+            firstName: firstName,
+            lastName: lastName,
+            phone: phone,
+            email: email,
+            password: password,
+            permissions: permissions,
+            modified_date: modified_date,
+            modified_by: modified_by,
+            paymentMethod: paymentMethod,
+            vehicles: vehicles,
+        });
+    }
+
+    updateUser(user) {
+        return this.db.updateUser(user);
+    }
+
+    authenticateUser() {
+        return this.db.authenticateUser;
+    }
+
+    /**
+     * Result is attached to the session as req.session.passport.user
+     *
+     * @returns {function} Callback function:
+     *                          @param {string} id - _id stored in mongodb
+     *                          @param {function} done - called internally by strategy implementation
+     * @memberof User
+     */
+    serializeUser() {
+        return function(user, done){
+            done(null, user._id);
+        };
+    }
+
+    /**
+     * In ID is used to find the user, which is then restored in req.user
+     *
+     * @returns {function} Async callback function:
+     *                          @param {string} id - Corresponds to _id in mongo database
+     *                          @param {function} done - called internally by strategy implementation
+     * @memberof User
+     */
+    deserializeUser() {
+        return async function(id, done){
+            const user = await this.getUserById(id);
+            done(null, user);
+        }.bind(this);
+    }
 }
 
 module.exports = User;
