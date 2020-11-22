daae0e3f02dbba43ac783e412e2ee7dc7ee56cac
Blaine <ClarkDevelopment@outlook.com>
2020-11-21 18:15:53 -0600

    Adds parking lot functionality and a new revenue report service

--- src/routes/parkingLot.js
+++ src/routes/parkingLot.js
@@ -2,7 +2,30 @@ var express = require("express");
 const app = require("../app");
 var router = express.Router();
 
-router.get("/", function (req, res) {});
+/**
+ * Used to get all users
+ * @name get/users
+ * @function
+ * @memberof module:routes/users~usersRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.get("/", async function (req, res) {
+  let lot;
+  try {
+    lot = await req.app.locals.parkingLot.getCurrentParkingLot();
+    console.log(lot);
+    if (lot == null) {
+      return res.status(404).json({ message: "Unable to retrieve parking lot" });
+    }
+    return res.json(lot);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+  return res.send(lot);  
+});
 
 router.post("/", function (req, res) {
   req.app.locals.parkingLot.addParkingLot();
